5a4ff7fecd64f6898d3140c6533d2552de05a887
Brale2002 <brallangombel@gmail.com>
2023-03-06 11:30:48 -0500

    primeros archivos

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+import { ContactUs } from './ContactUs/ContactUs'
+
+
+function App() {  
+
+  return (
+    <div className="App">
+      <div>        
+        <a href="https://reactjs.org" target="_blank">
+          <img src="https://i0.wp.com/blog.nu.com.co/wp-content/uploads/2021/10/Tumb-1Oct-19.png?fit=1280%2C748&ssl=1" className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <ContactUs />
+    </div>
+  )
+}
+
+export default App
